dc898f5c9eb5fde78ca71f01a5265b03ea62d04c
huanghaozi <huang_hao_zi@163.com>
2018-12-03 16:32:38 +0800

    开始施工

---
+++ db.py
@@ -0,0 +1,19 @@
+def gettable(database, table, colsNamesInJson):
+    try:
+        database.ping(reconnect=True)
+        cursor = database.cursor()
+        datas = []
+        data = {}
+        m = len(colsNamesInJson)
+        cursor.execute("select * from %s;" % (table))
+        results = cursor.fetchall()
+        cursor.close()
+        if not results:
+            return {'Empty': True}
+        for i in range(len(results)):
+            for j in range(m):
+                data[colsNamesInJson[j]] = results[i][j]
+            datas.append(data)
+        return datas
+    except:
+        return [{'Error': True}]
